fff6e0f3de2e19fee54d03e2d0b43f3c8039a5fc
adrianjost <adrianjost@hackedit.de>
2017-04-15 16:33:40 +0200

    META-Tags & Open Graph data

--- inc/html/head.php
+++ inc/html/head.php
@@ -1,10 +1,23 @@
 <!DOCTYPE html><html>
 <head>
-    <title>FastFeed</title>
     <meta name="theme-color" content="#00d9a3">
+    <title>FastFeed</title>
+    <meta name="description" content="A beautiful and fast RSS-Reader">
+    <meta name="author" content="Adrian Jost">
+    
+    <link rel="icon" type="image/png" href="inc/img/favicon/96.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="manifest" href="inc/manifest.json">
     <meta name="robots" content="noindex" />
+    
+    <!-- Twitter Card data -->
+    <meta name="twitter:card" value="summary">
+    <!-- Open Graph data -->
+    <meta property="og:title" content="FastFeed" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://fastfeed.hackedit.de" />
+    <meta property="og:image" content="https://fastfeed.hackedit.de/inc/img/favicon/192.png" />
+    <meta property="og:description" content="A beautiful and fast RSS-Reader" />
 	<!--Import Google Icon Font-->
 	<?php /* <link type="text/css" rel="stylesheet" href="inc/css/normalize.css"  media="screen,projection"/>
     <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <div class="content">
             <p><i>Fast</i><b>Feed</b> was developed to enjoy content of famous websites as fast as you have never seen it before. Yes, you can even use it on 2G-Networks with loading-times less than 15 seconds.</p>
             <p>We use modern web-technologies to load articles as fast as possible and every line of code was created with love and a lot of care. If you have any performance issues: please contact us!</p>
-            <p>And Yes, <i>Fast</i><b>Feed</b> is Open-Source!</br>Checkout here: <a href="" target="_blank">Github</a></p>
+            <p>And Yes, <i>Fast</i><b>Feed</b> is Open-Source!</br>Checkout here: <a href="https://github.com/adrianjost/fastfeed" target="_blank">Github</a></p>
             <div class="spende"><p>Do you enjoy <i>Fast</i><b>Feed</b>?</p><form onsubmit="location.href='https://www.paypal.me/AdrianJostHackedIT/'+((document.getElementById('euro').value=='')?'5':(document.getElementById('euro').value)); return false;"><label><input id="euro" placeholder="5" onkeypress="return (/^[0-9]$/.test(event.key))">&nbsp;€&nbsp;&nbsp;</label><button class="button-two" type="submit">Donate</button></form></div>
         </div>
     </div>
