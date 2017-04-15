e7ead28e3d6f03c2946267bfbda9c041fcf2e147
adrianjost <adrianjost@hackedit.de>
2017-04-15 14:42:16 +0200

    icon added

---
+++ inc/img/favicon/export.bat
@@ -0,0 +1,7 @@
+@echo off
+inkscape -z -w 36 -h 36 -e 36.png favicon.svg
+inkscape -z -w 48 -h 48 -e 48.png favicon.svg
+inkscape -z -w 72 -h 72 -e 72.png favicon.svg
+inkscape -z -w 96 -h 96 -e 96.png favicon.svg
+inkscape -z -w 144 -h 144 -e 144.png favicon.svg
+inkscape -z -w 192 -h 192 -e 192.png favicon.svg
