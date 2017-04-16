41603059dda2ed6b27e112bbee7c8575f72d02b4
adrianjost <adrianjost@hackedit.de>
2017-04-16 14:00:48 +0200

    added noscript stylesheet

--- inc/html/head.php
+++ inc/html/head.php
@@ -87,6 +87,7 @@ function jt(a,t){
 }
 fJS();*/
 </script>
+<noscript><link href="inc/css/styles.css" rel="stylesheet"></noscript>
 
 	<!--Let browser know website is optimized for mobile-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
