4660712f89bc2ca9eb8a6b8c08ce3629d19636a0
adrianjost <adrianjost@hackedit.de>
2017-04-17 14:25:05 +0200

    4

--- s/updatefeeds.php
+++ s/updatefeeds.php
@@ -10,24 +10,39 @@ DATA TABLES -> info.txt
 require("db.php");
 function update_feeds(){
     $feeds = get_feed_list();
+    $newfeeddata=[];
     foreach ($feeds as $feed) {
         $lastknownitemurl = get_last_feeditem_url($feed["feedid"]);
         if(!$lastknownitemurl){$lastknownitemurl = "";}
         $items = get_feed_and_parse($feed["feedurl"]);
         foreach($items as $item){
             $item["feedid"] = $feed["feedid"];
-            save_feed_item($item);
-}}}
+            $newfeeddata[] = $item;
+        }
+    }
+    function datesort($a, $b){
+        $a = intval($a["pubDate"]);
+        $b = intval($b["pubDate"]);
+        if ($a == $b) {return 0;}
+        return ($a < $b) ? -1 : 1;
+    }
+    usort($newfeeddata, "datesort");
+    
+    foreach ($newfeeddata as $item) {
+        unset($item["pubDate"]);
+        save_feed_item($item);
+}}
 
 function minimalize($str){
-    $str = trim($str)
+    $str = trim($str);
     //remove all html tags except <br><p><a>
-    strip_tags($str, '<br><p><a>');
+    $str = strip_tags($str,'<a>');
+    // encode html special chars
+    $str = htmlspecialchars($str);
     //remove images
     $str = preg_replace("/<img[^>]+\>/i", "(", $str); 
     //remove attributes from html-tags
     $str = preg_replace("/<([a-z][a-z0-9]*)[^>]*?(\/?)>/i",'<$1$2>', $str);
-    
     return $str;
 }
 
@@ -49,17 +64,21 @@ function get_feed_and_parse($url){
     $items=$xmlDoc->getElementsByTagName('item');
     //for ($i=0; $i<=2; $i++) {
     foreach ($items as $item) {
-        $item_link  = $item->getElementsByTagName('link')->item(0)->childNodes->item(0)->nodeValue;
-        $item_title = $item->getElementsByTagName('title')->item(0)->childNodes->item(0)->nodeValue;
-        $item_desc  = $item->getElementsByTagName('description')->item(0)->childNodes->item(0)->nodeValue;
+        $item_link  = $item->getElementsByTagName('link')->item(0)->nodeValue;
+        $item_title = $item->getElementsByTagName('title')->item(0)->nodeValue;
+        $item_desc  = $item->getElementsByTagName('description')->item(0)->nodeValue;
+        $item_pubDate  = $item->getElementsByTagName('pubDate')->item(0)->nodeValue;
         
+        $item_title = minimalize($item_title);
+        $item_desc  = minimalize($item_desc);
         if(strlen($item_title)<3){$item_title = "TL;DR";}
         if(strlen($item_desc)<5){$item_desc = "no description found";}
-        
+
         $entrys[] = [
-            "url"       => $item_link,
-            "title"     => minimalize($item_title),
-            "preview"   => minimalize($item_desc)
+            "url"       => trim($item_link),
+            "title"     => substr($item_title, 0, 200),
+            "preview"   => substr($item_desc, 0, 500),
+            "pubDate"   => strtotime($item_pubDate)
         ];
     }
     return $entrys;
